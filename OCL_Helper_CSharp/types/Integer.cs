fdfca284b6d112fff57f5b2ca3bd37bea7299c21
Karolina Raczkowska <karolina.raczkowska1@gmail.com>
2015-04-03 14:39:02 +0200

    ocl operations

---
+++ OCL_Helper_CSharp/types/Integer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OCL_Helper.types
+{
+    class Integer
+    {
+        int value;
+
+        Integer(int x)
+        {
+            value = x;
+        }
+
+        public Integer operator <(Integer i1, Integer i2)
+        {
+            return i1.value < i2.value ? i1 : i2;
+        }
+
+        public Integer operator >(Integer i1, Integer i2)
+        {
+            return i1.value > i2.value ? i1 : i2;
+        }
+    }
+}
