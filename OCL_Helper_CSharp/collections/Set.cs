1f4237e60d3e78510b989032e062e0a714f1c193
Karolina Raczkowska <karolina.raczkowska1@gmail.com>
2015-04-03 13:24:08 +0200

    ocl operations

---
+++ OCL_Helper_CSharp/collections/Set.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OCL_Helper.collections
+{
+    class Set<T>
+    {
+        List<T> list;
+
+        Set()
+        {
+            list = new List<T>();
+        }
+
+        public bool operator ==(Set<T> other, Set<T> set){//for the = operation
+            foreach(T t in list){
+                foreach(T t2 in other.list){
+                    if(!t.Equals(t2)){
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public bool operator !=(Set<T> other, Set<T> set)//for the <> operation
+        {
+            return other == set;
+        }
+
+        public int size()
+        {
+            return list.Count;
+        }
+
+        public bool includes(T elem)
+        {
+            return list.Contains(elem);
+        }
+
+        public bool excludes(T elem)
+        {
+            return !list.Contains(elem);
+        }
+
+        public int count(T elem)
+        {
+            int counter = 0;
+            foreach(T t in list)
+            {
+                if(t.Equals(elem))
+                {
+                    counter++;   
+                }
+            }
+            return counter;
+        }
+    }
+}
